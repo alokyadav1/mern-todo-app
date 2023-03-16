6232aa10ffa07140173c714da5b65ab2c486809b
Alok Yadav <alok.yadav8879@gmail.com>
2023-03-16 14:13:14 +0530

    minor change in task container

--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -29,7 +29,6 @@ function Task({ task, id}) {
             <div className="task-info text-slate-900 text-sm w-10/12">
                 <h4 className="task-title text-lg">{task.title}</h4>
                 <p className="task-description">{task.description}</p>
-                <p>{`${task.completed}`}</p>
             </div>
             <div className="remove-task text-sm text-white">
                 <DeleteIcon
