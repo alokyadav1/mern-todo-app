2cc4f51c23bf696f3cef94e75d69f4bf5a15c348
alokyadav1 <62539281+alokyadav1@users.noreply.github.com>
2023-03-24 20:54:25 +0530

    Update requireAuth.js

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,17 +1,14 @@
 import jwt from 'jsonwebtoken';
 const requireAuth = async (req, res, next) => {
-    console.log("requireAuth.js");
     const { authorization } = req.headers;
     if (!authorization) {
         return res.status(401).json({message:'Unauthorized'});
     }
     const token = authorization.split(' ')[1]+"";
     try {
-        console.log("token: ", token);
         const decoded =  jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         req.token = token;
-        console.log(req.user);
         next();
     } catch (error) {
         return res.status(500).json({message:error.message});
