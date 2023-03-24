030c4b627369e24fe8862852221e3bb876925b71
alokyadav1 <62539281+alokyadav1@users.noreply.github.com>
2023-03-24 20:58:49 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,11 +67,9 @@ const registerUser = async (req,res) => {
 
 //get user info
 const getUser = async (req,res) => {
-    console.log(req.user.id);
     const id = req.user.id
     try{
         const user = await userModel.find({_id:id})
-        console.log("User: ", user);
         res.status(200).json({user: user[0]})
     } catch(error){
         res.status(502).json({message: error.message})
